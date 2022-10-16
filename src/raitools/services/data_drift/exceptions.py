7d2dadab6f2e3f5744e8064a1c0e4110b408eb7e
Dylan Stark <dylan.stark@carelon.com>
2022-10-16 20:39:29 +0000

    Refactors exceptions.

--- src/raitools/services/data_drift/exceptions.py
+++ src/raitools/services/data_drift/exceptions.py
@@ -1,31 +1,13 @@
 """Exceptions for Data Drift."""
 
 
-from typing import Dict, Optional
-
-
 class BadPathToBundleError(Exception):
     """Bad path to bundle error."""
-
-    def __init__(self, message: str, info: Optional[Dict] = None) -> None:
-        """Initializes exception and sets extra info if provided."""
-        super().__init__(message)
-        self.info = info if info else {}
 
 
 class BadBundleZipFileError(Exception):
     """Bad bundle zip file error."""
 
-    def __init__(self, message: str, info: Optional[Dict] = None) -> None:
-        """Initializes exception and sets extra info if provided."""
-        super().__init__(message)
-        self.info = info if info else {}
-
 
 class BadJobConfigError(Exception):
     """Bad job config error."""
-
-    def __init__(self, message: str, info: Optional[Dict] = None) -> None:
-        """Initializes exception and sets extra info if provided."""
-        super().__init__(message)
-        self.info = info if info else {}
