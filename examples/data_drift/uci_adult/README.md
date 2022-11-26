e1bc1147f3e23d3894fd7a285c73c105cddc1fc8
Dylan Stark <dylan.stark@carelon.com>
2022-11-26 13:25:09 +0000

    Fixes up example.

--- examples/data_drift/uci_adult/README.md
+++ examples/data_drift/uci_adult/README.md
@@ -7,4 +7,4 @@
 - We downsample to 500 samples for baseline and 500 for test.
   We use a simple strategy of extracting 500-row blocks.
   No effort is made to control for proportions or representation.
-- No feature rank information is available. We set rank equal to position of the field in the documented list of names.
+- No feature importance information is available so we set them to arbitrary values.
--- examples/data_drift/uci_adult/uci_adult.py
+++ examples/data_drift/uci_adult/uci_adult.py
@@ -16,6 +16,7 @@ from raitools.services.data_drift.use_cases.process_bundle import process_bundle
 
 def create_bundle(
     job_config_path: Path,
+    feature_mapping_path: Path,
     baseline_data_path: Path,
     test_data_path: Path,
     output_path: Path,
@@ -24,6 +25,7 @@ def create_bundle(
     bundle_path = output_path / "bundle.zip"
     with ZipFile(bundle_path, "w") as zip_file:
         zip_file.write(job_config_path, arcname=job_config_path.name)
+        zip_file.write(feature_mapping_path, arcname=feature_mapping_path.name)
         zip_file.write(baseline_data_path, arcname=baseline_data_path.name)
         zip_file.write(test_data_path, arcname=test_data_path.name)
 
@@ -50,13 +52,18 @@ def run_uci_adult_example(bundle_path: Path, output_path: Path) -> None:
 def main() -> None:
     """Runs UCI Adult data set example."""
     job_config_path = Path("examples/data_drift/uci_adult/adult_job_config.json")
+    feature_mapping_path = Path("examples/data_drift/uci_adult/feature_mapping.csv")
     baseline_data_path = Path("examples/data_drift/uci_adult/data/adult_baseline.csv")
     test_data_path = Path("examples/data_drift/uci_adult/data/adult_test.csv")
     output_path = Path("scratch/examples/data_drift/uci_adult/")
     output_path.mkdir(parents=True, exist_ok=True)
 
     bundle_path = create_bundle(
-        job_config_path, baseline_data_path, test_data_path, output_path
+        job_config_path,
+        feature_mapping_path,
+        baseline_data_path,
+        test_data_path,
+        output_path,
     )
 
     run_uci_adult_example(bundle_path, output_path)
